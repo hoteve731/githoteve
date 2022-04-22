2cf1382aa82c14222bfa6670029bb216c1ed7f56
hoteve <fbghtks1000@gmail.com>
2022-04-22 16:10:00 +0900

    del쓰면버그나서컨틴유썼습니다

---
+++ 4주차 파이썬/python5.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+print ('5개의 메뉴를 추가해주세요! 5개가 입력되면 오늘의 메뉴를 추천해드려요. 동일한 메뉴는 안돼요!')
+print ()
+
+foodlist = []
+
+while True:
+    menu = input ('메뉴추가: ')
+
+    if menu not in foodlist:
+        foodlist.append(menu)
+        print ('현재 메뉴 수 = ', len(foodlist))
+    else:
+        print ('이미 있는 메뉴에요! 다른 메뉴를 입력해주세요') 
+        print ('현재 메뉴 수 = ', len(foodlist))
+        continue
+        
+    
+    if len(foodlist) > 4:
+        print()
+        print(foodlist)
+        print('과연 오늘의 메뉴는?')
+
+        print()
+        print('3')
+        time.sleep(1)
+        print('2')
+        time.sleep(1)
+        print('1')
+        time.sleep(1)
+        print()
+
+        randomnumber = [1,2,3,4,5]
+        foodnumber = random.choice(list(randomnumber))
+        
+
+        print('오늘의 메뉴는', int(foodnumber)+1, '번째 메뉴인', foodlist[foodnumber], '입니다' )
+        break
