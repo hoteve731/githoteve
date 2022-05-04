32c8b0a99f705a1d30cf830b34122805975fcc7d
hoteve <fbghtks1000@gmail.com>
2022-05-04 23:47:41 +0900

    1번텍스트파일라이트 수정

--- 5:5 파이썬 심화 과제/practice1.py
+++ 5:5 파이썬 심화 과제/practice1.py
@@ -1,21 +1,20 @@
 from unittest import result #이거 제가 쓴 적이 없는데 왜 생기는거죠???
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import date, datetime
 
 url = "https://music.bugs.co.kr/chart"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-
-titles = soup.select('p.title') #p 태그 안에 또 a태그로 들어있어서 a태그 써야하는줄 알고 삽질했습니다...ㅠㅠ
-
-print(datetime.today().strftime("%Y년 %m월 %d일의 벅스 실시간 차트 순위입니다.\n"))
-
 rank = 1
 
-for i in range(0,20): #리스트 길이만큼 할수도 있지만 너무 길어져서 20위로 잘랐어요
-    print(rank, "위: ",titles[i].text) #아직도 이렇게 간단하게 되네? 라고 믿기지가 않는 코드
+results = soup.findAll('p','title') 
+openfile = open("practice1.txt",'w', encoding="UTF-8")
+openfile.write(str(datetime.now())+"의 벅스 실시간 차트 순위입니다.\n")
+
+for title in results: 
+    openfile.write( str(rank)+ "위: "+ str(title.get_text())+"\n") 
     rank = rank + 1
 
 
