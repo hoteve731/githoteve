38e962ba6b5dd904427c19d53fe9f3a96e56a8d2
hoteve <fbghtks1000@gmail.com>
2022-05-05 14:08:21 +0900

    추가과제재업

---
+++ 5월5일 파이썬 기초 추가과제/2.py
@@ -0,0 +1,9 @@
+x = int((input('Enter a value for x: ')))
+
+def math(x):  
+    return ((3*(x**5)+2*(x**4)-5*(x**3)-(x**2)+(7*x)-6))
+
+print ('Polynomial for x= ' + str(x) +': '+str(math(x)))
+
+#이 문제는 (굳이) 함수를 사용해봤는데 이렇게 하는게 맞는지는 잘 모르겠네요ㅎㅎ 괜히 더 복잡해진 느낌이라...
+
