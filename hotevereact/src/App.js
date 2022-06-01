5d94eb3cecfbf31b381a319f84dffddd53b69faf
hosan <fbghtks1000@gmail.com>
2022-06-01 23:44:59 +0900

    리액트진짜최종

--- hotevereact/src/App.js
+++ hotevereact/src/App.js
@@ -5,7 +5,6 @@ import { Link, Route, Routes } from "react-router-dom";
 import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
 import Home from './pages/Home';
-// import menubar from './pages/Menubar';
 
 const App = () => {
   return(
@@ -13,21 +12,15 @@ const App = () => {
      <div className='nav'>
       <button className='navbutton'><Link to = '/page1'>페이지1</Link></button>
       <button className='navbutton'><Link to = '/page2'>페이지2</Link></button>
-      <button className='navbutton'><Link to = '/home'>홈</Link></button>
+      <button className='navbutton'><Link to = '/'>홈</Link></button>
     </div>
   
-
     <Routes>
-        <Route path = '/home' element = {<Home/>} />
+        <Route path = '/' element = {<Home/>} />
         <Route path = '/page1' element = {<Page1/>} />
         <Route path = '/page2' element = {<Page2/>}/>
     </Routes>
-
-   
-   
-      
     </>
-    
   );
 }
 
--- hotevereact/src/Music.js
+++ hotevereact/src/Music.js
@@ -5,9 +5,12 @@ function Music() {
   return(
     <div className='musiclist'>
       <h1 className='hi'>저의 최애 곡들을 소개합니다!</h1>
-      <li>{music[0]}</li>
-      <li>{music[1]}</li>
-      <li>{music[2]}</li>
+      <div className='musicname'>
+        <li>{music[0]}</li>
+        <li>{music[1]}</li>
+        <li>{music[2]}</li>
+      </div>
+      
 
       <button className='button' onClick = {()=>{
           setMusic(["이걸 클릭하네",'(G)-Idle - Tomboy','Le Sserafim - FEARLESS']);
